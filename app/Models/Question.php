1b2a026c0e8491858d2bb722875e08ef56d3bcfc
Sahil <dhimansahil610@gmail.com>
2021-02-13 22:54:47 +0530

    Generate Question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+    protected $fillable=['title','body'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] =$value;
+        $this->attributes['slug']=str_slug($value);
+    }
 }
